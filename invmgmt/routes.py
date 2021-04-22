75b5769447b6600e6e9d87ac1485169030d23d3c
spectakural <this.is.kural987@gmail.com>
2021-04-22 20:29:41 +0530

    1.6 (i guess)

--- invmgmt/routes.py
+++ invmgmt/routes.py
@@ -1,10 +1,23 @@
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for,redirect
 from invmgmt import app
+import os
 
 @app.route('/')
 @app.route('/home')
+@app.route('/index')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 
-    
+@app.route('/importexcel', methods=["POST", "GET"])    
+def importexcel():
+    if request.method == "POST":
+        req=request.form
+
+        excel = request.files['excel']
+        excel.save(f"./invmgmt/static/{excel.filename}")
+        print(excel)
+        return None
+    else:
+        return render_template('importexcel.html')
+
